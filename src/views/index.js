dfd0e3447cf93ece1f967d82de481cf2926bee31
Spencer Smith <dontaskspencer@gmail.com>
2017-10-20 10:34:28 -0600

    Style changes

--- src/views/index.js
+++ src/views/index.js
@@ -1,1 +1,15 @@
-console.log('index here...');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="content">
+        <h1 className="title">Start with React</h1>
+      </div>
+    );
+  }
+}
+
+var root = document.querySelector('#root');
+ReactDOM.render(<App/>, root);
